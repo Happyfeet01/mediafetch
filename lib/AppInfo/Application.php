b665e1363cb1c87c3dbce9b0f0250b3cc4170eb6
Happyfeet01 <3295104+Happyfeet01@users.noreply.github.com>
2026-08-26 07:57:07 +0200

    Build MediaFetch aria2 config with correct app hooks

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -3,16 +3,18 @@
 namespace OCA\NCDownloader\AppInfo;
 
 use OCA\NCDownloader\Aria2\Aria2;
+use OCA\NCDownloader\Db\Settings;
 use OCA\NCDownloader\Http\Client;
 use OCA\NCDownloader\Tools\Helper;
-use OCA\NCDownloader\Db\Settings;
 use OCA\NCDownloader\Ytdl\Ytdl;
+use OCP\App\IAppManager;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootstrap;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IRegistrationContext;
-use Symfony\Component\DomCrawler\Crawler;
+use OCP\IConfig;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\DomCrawler\Crawler;
 
 class Application extends App implements IBootstrap
 {
@@ -44,14 +46,60 @@ class Application extends App implements IBootstrap
         }
     }
 
-    public function boot(IBootContext $c): void
+    public function boot(IBootContext $context): void
     {
         $user = Helper::getUser();
         $uid = $user ? $user->getUID() : '';
-        $container = $c->getAppContainer();
+        $container = $context->getAppContainer();
 
         $container->registerService(Aria2::class, function (ContainerInterface $c) use ($uid) {
-            return new Aria2(Helper::getAria2Config($uid));
+            $settings = new Settings($uid);
+            $options = [];
+            $options['seed_time'] = $settings->get('ncd_seed_time');
+            $options['seed_ratio'] = $settings->get('ncd_seed_ratio');
+
+            $customSettings = $settings->getAria2();
+            if (is_array($customSettings)) {
+                $options = array_merge($customSettings, $options);
+            }
+
+            $proxy = $settings->get('ncd_download_proxy');
+            if ($proxy) {
+                $options['all-proxy'] = $proxy;
+            }
+
+            $config = $c->get(IConfig::class);
+            $appManager = $c->get(IAppManager::class);
+            $dataDir = (string) $config->getSystemValue('datadirectory');
+            $appPath = (string) $appManager->getAppPath(self::APP_ID);
+            $aria2Conf = Helper::getSettings('global_aria2_config', [], Settings::TYPE['SYSTEM']);
+            if (!is_array($aria2Conf)) {
+                $aria2Conf = [];
+            }
+
+            // Supplying the hooks here prevents the legacy Aria2 class defaults
+            // from falling back to /apps/ncdownloader after the rebrand.
+            $aria2Conf += [
+                'on-download-complete' => $appPath . '/hooks/completeHook.sh',
+                'on-download-start' => $appPath . '/hooks/startHook.sh',
+            ];
+
+            $token = Helper::getAdminSettings('ncd_aria2_rpc_token');
+            $rpcHost = Helper::getAdminSettings('ncd_aria2_rpc_host');
+            $rpcPort = Helper::getAdminSettings('ncd_aria2_rpc_port');
+            $binary = Helper::getAdminSettings('ncd_aria2_binary');
+
+            return new Aria2([
+                'dir' => Helper::getRealDownloadDir(),
+                'torrentsDir' => Helper::getRealTorrentsDir(),
+                'confDir' => $dataDir . '/aria2',
+                'token' => $token ?: 'ncdownloader123',
+                'settings' => $options,
+                'rpcHost' => $rpcHost ?: '127.0.0.1',
+                'rpcPort' => $rpcPort ?: '6800',
+                'binary' => $binary,
+                'aria2Conf' => $aria2Conf,
+            ]);
         });
 
         $container->registerService(Ytdl::class, function (ContainerInterface $c) use ($uid) {
