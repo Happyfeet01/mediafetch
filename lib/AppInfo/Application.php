4e718b90157f1b153198bdcaa4cb10b885eea1e9
Happyfeet01 <3295104+Happyfeet01@users.noreply.github.com>
2026-08-26 07:45:09 +0200

    Use MediaFetch app id in bootstrap

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -16,24 +16,25 @@ use Psr\Container\ContainerInterface;
 
 class Application extends App implements IBootstrap
 {
-    public function __construct(array $urlParams = array())
+    public const APP_ID = 'mediafetch';
+
+    public function __construct(array $urlParams = [])
     {
-        parent::__construct('ncdownloader', $urlParams);
+        parent::__construct(self::APP_ID, $urlParams);
     }
+
     public function register(IRegistrationContext $context): void
     {
         $context->registerService(Client::class, function () {
-            $options = [
-                'ipv4' => true,
-            ];
-            return Client::create($options);
+            return Client::create(['ipv4' => true]);
         });
+
         $context->registerService(Crawler::class, function () {
             return new Crawler();
         });
+
         $sites = Helper::getSearchSites();
         foreach ($sites as $site) {
-            //fully qualified class name
             $className = $site['class'];
             $context->registerService($className, function (ContainerInterface $container) use ($className) {
                 $crawler = $container->get(Crawler::class);
@@ -46,26 +47,23 @@ class Application extends App implements IBootstrap
     public function boot(IBootContext $c): void
     {
         $user = Helper::getUser();
-        $uid = ($user) ? $user->getUID() : '';
-        //$settings = new Settings($uid);
-        //$userFolder = Helper::getUserFolder($uid);
-        $context = $c->getAppContainer();
+        $uid = $user ? $user->getUID() : '';
+        $container = $c->getAppContainer();
 
-        $context->registerService(Aria2::class, function (ContainerInterface $c) use ($uid) {
-            $config = Helper::getAria2Config($uid);
-            return new Aria2($config);
-        });
-        $context->registerService(Ytdl::class, function (ContainerInterface $c) use ($uid) {
-            $config = Helper::getYtdlConfig($uid);
-            return new Ytdl($config);
+        $container->registerService(Aria2::class, function (ContainerInterface $c) use ($uid) {
+            return new Aria2(Helper::getAria2Config($uid));
         });
 
-        $context->registerService(Settings::class, function (ContainerInterface $c) use ($uid) {
+        $container->registerService(Ytdl::class, function (ContainerInterface $c) use ($uid) {
+            return new Ytdl(Helper::getYtdlConfig($uid));
+        });
+
+        $container->registerService(Settings::class, function (ContainerInterface $c) use ($uid) {
             return new Settings($uid);
         });
-        $context->registerService('uid', function (ContainerInterface $c) use ($uid) {
+
+        $container->registerService('uid', function (ContainerInterface $c) use ($uid) {
             return $uid;
         });
-        //$context->injectFn([$this, 'registerSearchProviders']);
     }
 }
