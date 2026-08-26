7c5c1278f88d82a2eaf7044e6ed7428aa0db947d
Happyfeet01 <3295104+Happyfeet01@users.noreply.github.com>
2026-08-26 07:51:43 +0200

    Update yt-dlp system info routes for MediaFetch

--- src/components/systemInfo.vue
+++ src/components/systemInfo.vue
@@ -4,17 +4,13 @@
         <div class="system-info">
             <div class="system-info-item">
                 <div class="system-info-item-label">Aria2 Version: </div>
-                <div class="system-info-item-value"><span class="version">{{ aria2Ver }}</span>
-                </div>
+                <div class="system-info-item-value"><span class="version">{{ aria2Ver }}</span></div>
             </div>
             <div class="system-info-item">
-                <div class="system-info-item-label">Yt-dlp Version: </div>
+                <div class="system-info-item-label">yt-dlp Version: </div>
                 <div class="system-info-item-value"><span class="version">{{ ytdVer }}</span>
                     <actionButton action="check" btnType="ytd" @clicked="checkUpdate" enableLoading="true"
-                        className="check-button">
-                        {{
-        ytdBtn
-                        }}</actionButton>
+                        className="check-button">{{ ytdBtn }}</actionButton>
                 </div>
             </div>
         </div>
@@ -23,16 +19,14 @@
 <script>
 import helper from "../utils/helper";
 import actionButton from "./actionButton";
-const ARIA2_CHECK_URL = "/apps/ncdownloader/aria2/release/check";
-const ARIA2_UPDATE_URL = "/apps/ncdownloader/aria2/release/update";
-const YTD_CHECK_URL = "/apps/ncdownloader/ytdl/release/check";
-const YTD_UPDATE_URL = "/apps/ncdownloader/ytdl/release/update";
+
+const YTD_CHECK_URL = "/apps/mediafetch/ytdl/release/check";
+const YTD_UPDATE_URL = "/apps/mediafetch/ytdl/release/update";
 
 export default {
     name: "systemInfo",
     data() {
         return {
-            aria2Btn: "Check for Update",
             ytdBtn: "Check for Update",
         };
     },
@@ -40,66 +34,45 @@ export default {
         actionButton
     },
     computed: {
-        aria2Ver: {
-            get() {
-                return this.$props.aria2Version
-            },
-            set(value) {
-                this.$props.aria2Version = value
-                //this.$emit("update:aria2Version", value)
-            }
+        aria2Ver() {
+            return this.$props.aria2Version;
         },
         ytdVer: {
             get() {
-                return this.$props.ytdVersion
+                return this.$props.ytdVersion;
             },
             set(value) {
-                this.$props.ytdVersion = value
-                //this.$emit("update:ytdVersion", value)
+                this.$emit("update:ytdVersion", value);
             }
         }
     },
     methods: {
         checkUpdate(event, $vm) {
-            const { btnType, action } = $vm.$props;
-            const path = action === "check" ? (btnType === "aria2" ? ARIA2_CHECK_URL : YTD_CHECK_URL) : (btnType === "aria2" ? ARIA2_UPDATE_URL : YTD_UPDATE_URL);
+            const { action } = $vm.$props;
+            const path = action === "check" ? YTD_CHECK_URL : YTD_UPDATE_URL;
             helper
                 .httpClient(helper.generateUrl(path))
                 .setMethod("GET")
                 .setHandler((data) => {
                     $vm.loading = false;
                     if (data.status) {
-                        helper.info(data.message)
-                        //update button text
-                        if (action == "check") {
-                            if (btnType == "ytd") {
-                                this.ytdBtn = "Update"
-                            } else {
-                                this.aria2Btn = "Update"
-                            }
-                            $vm.$props.action = "update"
+                        helper.info(data.message);
+                        if (action === "check") {
+                            this.ytdBtn = "Update";
+                            $vm.$props.action = "update";
                         } else {
-                            if (btnType == "ytd") {
-                                this.ytdBtn = "Check for Update"
-                            } else {
-                                this.aria2Btn = "Check for Update"
-                            }
-                            $vm.$props.action = "check"
+                            this.ytdBtn = "Check for Update";
+                            $vm.$props.action = "check";
                             if (data.data) {
-                                if (btnType == "ytd") {
-                                    this.ytdVer = data.data
-                                } else if (btnType == "aria2") {
-                                    this.aria2Ver = data.data
-                                }
+                                this.ytdVer = data.data;
                             }
                         }
                     } else {
-                        helper.info(data.message)
+                        helper.info(data.message);
                     }
                 })
                 .send();
         },
-
     },
     props: {
         aria2Version: {
@@ -118,7 +91,6 @@ export default {
     display: flex;
     flex-direction: column;
     margin-top: 10px;
-
 
     .system-info-item {
         display: flex;
